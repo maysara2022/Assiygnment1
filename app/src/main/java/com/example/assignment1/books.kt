20b54dde366f356ee4328eeb065dd3a438f6803f
maysara2022 <maysaranasser8@gmail.com>
2022-05-21 21:20:27 +0300

    Assignment1

---
+++ app/src/main/java/com/example/assignment1/books.kt
@@ -0,0 +1,9 @@
+package com.example.assignment1
+
+class books(
+    var BookName: String = "",
+    var BookAuthor: String = "",
+    var Price: String = "",
+    var id: String = "",
+    var BookLunch: String = ""
+)
